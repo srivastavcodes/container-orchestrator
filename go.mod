feca82d174fb4a7ec1efc1dc635ee30057d0e89b
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-10-28 21:50:43 +0530

    add: basic code structure and task skeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module orchestrator
 
 go 1.24
+
+require (
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
---
+++ task/task.go
@@ -0,0 +1,62 @@
+package task
+
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+// Task represents a task that the user wants to run on our cluster.
+type Task struct {
+	// ID is the 128 bit universally unique identifier to distinguish between
+	// individual tasks.
+	ID          uuid.UUID
+	ContainerID string
+
+	// Name is the name of the task for better readability.
+	Name string
+
+	// State represents the current state of the task. See State more info.
+	State State
+
+	// Image the docker image a task will use.
+	Image string
+	CPU   float64
+
+	// Memory and Disk help the system identify the number of resources a task
+	// needs.
+	Memory int64
+	Disk   int64 // See Memory for docs.
+
+	// ExposedPorts and PortBindings are used by docker to ensure the machine
+	// allocates the proper network ports for the task.
+	ExposedPorts nat.PortSet
+	PortBindings map[string]string // See ExposedPorts for docs.
+
+	// RestartPolicy tells the system what to do when a task stops or fails
+	// unexpectedly.
+	RestartPolicy string
+
+	// StartTime and FinishTime lets the user know when the task started and
+	// when it stopped.
+	StartTime  time.Time
+	FinishTime time.Time // see StartTime for docs.
+}
+
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	TimeStamp time.Time
+	Task      Task
+}
