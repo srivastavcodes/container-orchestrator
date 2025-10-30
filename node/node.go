a517ab953d5d663debddef8cb70d63d6a7ba652f
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-10-30 07:46:52 +0530

    better readability

--- node/node.go
+++ node/node.go
@@ -4,28 +4,19 @@ package node
 // machine's resources, both total and allocated, to help the scheduler make
 // decisions about task placement.
 type Node struct {
-	// Name is the identifier used to identify an individual node.
-	Name string
-
-	// Ip is the IP address of the node in the cluster.
-	Ip string
-
-	// Cores is the total number of CPU cores available on the node.
-	Cores int
-
-	// Memory is the total amount of memory available on the node.
-	Memory int
-
-	// MemoryAllocated is the amount of memory currently allocated
-	// to tasks.
-	MemoryAllocated int
-
-	// Disk is the total amount of disk space available on the node.
-	Disk int
+	Name   string // Name is the identifier used to identify an individual node.
+	Ip     string // Ip is the IP address of the node in the cluster.
+	Cores  int    // Cores is the total number of CPU cores available on the node.
+	Disk   int    // Disk is the total amount of disk space on the node.
+	Memory int    // Memory is the total amount of memory available on the node.
 
 	// DiskAllocated is the amount of disk space currently allocated
 	// to tasks.
 	DiskAllocated int
+
+	// MemoryAllocated is the amount of memory currently allocated
+	// to tasks.
+	MemoryAllocated int
 
 	// Role defines the role of the node in the cluster. Worker-Node,
 	// Manager-Node
